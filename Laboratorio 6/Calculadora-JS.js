60e77bd5feb5eaa0328c251630160d4c5dd57671
Zavalota <netbeans1314@gmail.com>
2026-09-22 17:15:15 -0600

    Laboratorio 6 Calculadora JS RO

---
+++ Laboratorio 6/Calculadora-JS.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresa tu nombre: ");
+
+let numero1=Number(prompt("Ingresa numero 1: "));
+let numero2=Number(prompt("Ingresa numero 2: "));
+console.log("Hola "+nombre+" un gusto!!! tus resultados son: ");
+console.log("La suma es: ", numero1 + numero2);
+console.log("La resta es: ", numero1 - numero2);
+console.log("La division es: ", numero1 / numero2);
+console.log("La multiplicacion es es: ", numero1 * numero2);
